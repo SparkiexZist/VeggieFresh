227abfbbd06c91a731e4707e6d22294c098a0576
Sparkie <pug_01@hotmail.com>
2021-09-17 13:34:18 +0700

    Add orderRepository.

--- src/main/java/th/ac/ku/restaurant/repository/OrderRepository.java
+++ src/main/java/th/ac/ku/restaurant/repository/OrderRepository.java
@@ -2,12 +2,12 @@ package th.ac.ku.restaurant.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import th.ac.ku.restaurant.model.User;
+import th.ac.ku.restaurant.model.VegOrder;
 
 import java.util.UUID;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, UUID>
+public interface OrderRepository extends JpaRepository<VegOrder, UUID>
 {
-    User findByUsername(String username);
+
 }
